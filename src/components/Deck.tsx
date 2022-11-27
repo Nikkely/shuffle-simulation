71150a7089dd11c4db413e2b4035e485643507b8
nikkely <umivudau153@gmail.com>
2022-11-27 22:49:51 +0900

    feat: add history

--- src/components/Deck.tsx
+++ src/components/Deck.tsx
@@ -21,8 +21,10 @@ function Deck(props: DeckProps) {
         _cards.push({ key: i, x: i, color: makeGradation(i, props.num) })
     }
     const [cards, setCards] = useState<CardParams[]>(_cards)
-    const reflect = (shuffle: (c: CardParams[]) => ShuffledType[]) => {
+    const [history, setHistory] = useState<string[]>([])
+    const reflect = (name: string, shuffle: (c: CardParams[]) => ShuffledType[]) => {
         return () => {
+            setHistory([...history, name])
             cards.sort((a, b) => a.x - b.x)
             const shuffled = shuffle(cards)
             shuffled.sort((a: ShuffledType, b: ShuffledType) => a.old - b.old)
@@ -39,11 +41,12 @@ function Deck(props: DeckProps) {
     return (
         <div className="Deck">
             <ButtonGroup variant="contained" aria-label="shuffle pattern">
-                <Button variant="outlined" onClick={reflect(HinduShuffle)}>hindu</Button>
-                <Button variant="outlined" onClick={reflect(ShotgunShuffle(7))}>shotgun(7)</Button>
-                <Button variant="outlined" onClick={reflect(FarrowShuffle)}>farrow</Button>
+                <Button variant="outlined" onClick={reflect("Hindu", HinduShuffle)}>hindu</Button>
+                <Button variant="outlined" onClick={reflect("Shotgun", ShotgunShuffle(7))}>shotgun(7)</Button>
+                <Button variant="outlined" onClick={reflect("Farrow", FarrowShuffle)}>farrow</Button>
             </ButtonGroup>
             <div style={{
+                height: "30px",
                 margin: "10px",
             }}>
                 {
@@ -55,6 +58,9 @@ function Deck(props: DeckProps) {
                         />)
                     })
                 }</div>
+            <p> 
+                {displayHistory(history)}
+            </p>
         </div >
     );
 }
@@ -74,6 +80,24 @@ function makeGradation(x: number, max: number): string {
     }
     const r = range(255 - (x * 255) / max)
     return toColorCord(r, 200, range(255 - r))
+}
+
+function displayHistory(history: string[]): string {
+    if (history.length == 0) {
+        return "not shuffled yet"
+    }
+
+    const work: string[] = []
+    var counter = 1
+    for (var i: number = 0; i < history.length - 1; i++) {
+        if (history[i] !== history[i + 1]) {
+            work.push(history[i] + " x" + String(counter))
+            counter = 0
+        }
+        counter++
+    }
+    work.push(history[history.length-1] + " x" + String(counter))
+    return work.join(" > ")
 }
 
 export default Deck;
